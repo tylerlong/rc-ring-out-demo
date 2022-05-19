e7c5a0fd8490ad7e310753c1f80805ee70d56df6
Tyler Long <tyler4long@gmail.com>
2022-05-19 11:14:11 -0700

    Hello RingCentral

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world');
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+  console.log(rc.token?.access_token);
+  await rc.revoke();
+};
+
+main();
